d71650949e7f01d0db8934d2516139d768818a40
kraanzu <kraanzu@gmail.com>
2022-02-27 14:05:16 +0530

    chore: Add docs for emits

--- textual_extras/events/events.py
+++ textual_extras/events/events.py
@@ -2,12 +2,20 @@ from textual.events import Event
 
 
 class TextChanged(Event):
+    """Emmited when the text is changed in the widget"""
+
     pass
 
 
 class PyperclipError(Event):
+    """Emmited when the text could not be pasted from system clipboard"""
+
     pass
 
 
 class InvalidInputAttempt(Event):
+    """
+    Emitted when a letter in blackist or a letter not in whitelist was being tried to push in the widget
+    """
+
     pass
