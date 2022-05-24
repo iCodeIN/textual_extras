674bbdb2ef26e41a08e2359140952fb4b74e7df8
kraanzu <kraanzu@gmail.com>
2022-05-24 17:01:07 +0530

    feat: Improvents + Add and Edit new options

---
+++ textual_extras/widgets/list_edit.py
@@ -0,0 +1,40 @@
+from textual import events
+
+from . import SingleLevelTreeEdit
+from ..events import ListItemSelected
+
+
+class ListEdit(SingleLevelTreeEdit):
+
+    async def on_key(self, event: events.Key) -> None:
+        if self.editing:
+            if event.key == "escape":
+                self.unfocus_option()
+            else:
+                await self.options[self.selected].handle_keypress(event.key)
+
+        else:
+            match event.key:
+                case "j" | "down":
+                    self.move_cursor_down()
+                case "k" | "up":
+                    self.move_cursor_up()
+                case "g" | "home":
+                    self.move_cursor_to_top()
+                case "G" | "end":
+                    self.move_cursor_to_bottom()
+                case "i":
+                    self.focus_option()
+                case "a":
+                    self.add_option_below()
+                case "A":
+                    self.add_option_at_end()
+                case "enter":
+                    await self.emit(
+                        ListItemSelected(
+                            self,
+                            self.options[self.selected].value,
+                        )
+                    )
+
+        self.refresh()
